664e2d705bcf118376a636cfc2f622fa9c271486
Anamol <anmoldkl971@gmail.com>
2023-02-08 13:36:28 +0545

    Anamol's Change

--- Solana Analysis/Solana Live Price.py
+++ Solana Analysis/Solana Live Price.py
@@ -1,18 +1,16 @@
 import requests
+import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-headers = {'Accepts': 'application/json', 'X-CMC_Pro_API_Key': 'e3763607-e592-4ba9-b6d5-3769d4a54290'}
-params = {'symbol': 'BTC', 'convert': 'USD'}
+def get_sol_price():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return float(data['price'])
 
-data = requests.get(url, headers=headers, params=params).json()
-d = data['data'][list(data['data'].keys())[0]]
-quote = d['quote']['USD']
+previous_price = None
 
-symbol = d['symbol']
-price = "${:.2f}".format(quote['price'])
-volume = "${:,.2f}".format(quote['volume_24h'])
-change = "{:.2f}%".format(quote['volume_change_24h'])
-percent1h = "{:.2f}%".format(quote['percent_change_1h'])
-percent24h = "{:.2f}%".format(quote['percent_change_24h'])
-
-print(f"\nSymbol: {symbol}\nPrice: {price}\n\n24_Hour\nTraded: {volume}\nChange: {change}\n\nPercent\n1 Hour: {percent1h}\n24 Hour: {percent24h}")
+while True:
+    sol_price = get_sol_price()
+    if sol_price != previous_price:
+        print("Latest SOL Price:", sol_price)
+        previous_price = sol_price
